30ae3bd6220fe488dfd67a475edd4ff864aee255
JJiWoNing <jjiwoning12@gmail.com>
2023-05-23 15:45:27 +0900

    feat: JPA Auditing 적용

--- src/main/java/com/example/springpractice/SpringPracticeApplication.java
+++ src/main/java/com/example/springpractice/SpringPracticeApplication.java
@@ -2,7 +2,9 @@ package com.example.springpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringPracticeApplication {
 
--- src/main/java/com/example/springpractice/domain/Member.java
+++ src/main/java/com/example/springpractice/domain/Member.java
@@ -3,9 +3,15 @@ package com.example.springpractice.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class Member {
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
@@ -14,4 +20,11 @@ public class Member {
     private String name;
 
     private String nickname;
+
+    @Builder
+    public Member(Long id, String name, String nickname) {
+        this.id = id;
+        this.name = name;
+        this.nickname = nickname;
+    }
 }
