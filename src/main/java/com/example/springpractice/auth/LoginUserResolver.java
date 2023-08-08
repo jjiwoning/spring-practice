5b0c607a720dad6d525fefc99ffd0d21c848702a
JJiWoNing <jjiwoning12@gmail.com>
2023-08-08 21:55:12 +0900

    feat: Token 관련 Annotation, ArgumentResolver 추가

---
+++ src/main/java/com/example/springpractice/auth/LoginUserResolver.java
@@ -0,0 +1,22 @@
+package com.example.springpractice.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginUserResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(LoginUser.class) != null &&
+                parameter.getParameterType().equals(LoginUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        return request.getAttribute(UserConst.LOGIN_TOKEN);
+    }
+}
