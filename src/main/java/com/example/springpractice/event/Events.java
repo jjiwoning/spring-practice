a14a8bfd02abae2a1a25f4a5ae60e7e019ec9dc0
JJiWoNing <jjiwoning12@gmail.com>
2023-11-13 18:03:04 +0900

    feat: Events 구현

---
+++ src/main/java/com/example/springpractice/event/Events.java
@@ -0,0 +1,18 @@
+package com.example.springpractice.event;
+
+import org.springframework.context.ApplicationEventPublisher;
+
+public class Events {
+
+	private static ApplicationEventPublisher applicationEventPublisher;
+
+	static void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		Events.applicationEventPublisher = applicationEventPublisher;
+	}
+
+	public static void raise(Object event) {
+		if (applicationEventPublisher != null) {
+			applicationEventPublisher.publishEvent(event);
+		}
+	}
+}
