72b0119fd345c4e9fe599081d62c780902d22b7e
JJiWoNing <jjiwoning12@gmail.com>
2024-01-03 00:22:05 +0900

    clean architecture: JPA Repository 구현

---
+++ src/main/java/com/example/springpractice/buckpal/account/adapter/out/persistence/AccountRepository.java
@@ -0,0 +1,6 @@
+package com.example.springpractice.buckpal.account.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface AccountRepository extends JpaRepository<AccountJpaEntity, Long> {
+}
