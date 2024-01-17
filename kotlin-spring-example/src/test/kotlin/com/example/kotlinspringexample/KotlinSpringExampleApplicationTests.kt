243523f9087bc739dfc62f49910a3c72203c6be9
JJiWoNing <jjiwoning12@gmail.com>
2024-01-18 00:53:00 +0900

    spring-kotlin 모듈 추가

---
+++ kotlin-spring-example/src/test/kotlin/com/example/kotlinspringexample/KotlinSpringExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
