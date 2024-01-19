26fd55f170d77edcf4d908aac25b9bcde3eba828
JJiWoNing <jjiwoning12@gmail.com>
2024-01-19 23:54:35 +0900

    chore: kotlin all open gradle 세팅

--- kotlin-spring-example/build.gradle.kts
+++ kotlin-spring-example/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.8.0"
     kotlin("plugin.spring") version "1.8.0"
     kotlin("plugin.jpa") version "1.8.0"
+    kotlin("plugin.allopen") version "1.8.0"
 }
 
 group = "com.example"
@@ -40,4 +41,10 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperclass")
 }
