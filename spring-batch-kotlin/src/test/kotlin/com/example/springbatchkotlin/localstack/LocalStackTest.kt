9a47b27cbd1eb48fbcd140354502e00474e97b4b
JJiWoNing <jjiwoning12@gmail.com>
2024-03-24 21:10:53 +0900

    test: gradle 변경된 버전에 맞춰 LocalStackTest 수정

--- spring-batch-kotlin/src/test/kotlin/com/example/springbatchkotlin/localstack/LocalStackTest.kt
+++ spring-batch-kotlin/src/test/kotlin/com/example/springbatchkotlin/localstack/LocalStackTest.kt
@@ -1,29 +1,22 @@
 package com.example.springbatchkotlin.localstack
 
-import com.amazonaws.auth.AWSCredentialsProviderChain
-import com.amazonaws.client.builder.AwsClientBuilder
-import com.amazonaws.regions.ServiceAbbreviations.S3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import org.junit.Rule
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.localstack.LocalStackContainer
+import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.shaded.org.apache.commons.io.IOUtils
-import org.testcontainers.utility.DockerImageName
 import java.io.IOException
 
 @Testcontainers
 @DisplayName("S3 LocalStack 테스트")
 class LocalStackTest {
 
-    private val LOCALSTACK_NAME: DockerImageName = DockerImageName.parse("localstack/localstack")
-
-    @JvmField
-    @Rule
-    var localStackContainer: LocalStackContainer = LocalStackContainer(LOCALSTACK_NAME)
-        .withServices(LocalStackContainer.EnabledService { S3 })
+    @Container
+    var localStackContainer: LocalStackContainer = LocalStackContainer()
+        .withServices(LocalStackContainer.Service.S3)
 
     @Test
     @DisplayName("s3 업로드 테스트")
@@ -31,8 +24,8 @@ class LocalStackTest {
     fun test() {
         //localstack 컨테이너로 AmazonS3 설정 값에 추가.
         val amazonS3 = AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(localStackContainer.endpoint.toString(), localStackContainer.region))
-            .withCredentials(AWSCredentialsProviderChain())
+            .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(LocalStackContainer.Service.S3))
+            .withCredentials(localStackContainer.defaultCredentialsProvider)
             .build()
 
         //S3 인스턴스로 버킷 생성 및 put, get test
