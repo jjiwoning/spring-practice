6ae912bdd8891eec6cf6d27a48eb957d83a3b9b6
JJiWoNing <jjiwoning12@gmail.com>
2024-05-11 21:50:32 +0900

    chore: spring data envers, querydsl dependency

--- kotlin-spring-example/build.gradle.kts
+++ kotlin-spring-example/build.gradle.kts
@@ -3,10 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.1"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.spring") version "1.8.0"
-    kotlin("plugin.jpa") version "1.8.0"
-    kotlin("plugin.allopen") version "1.8.0"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
+    kotlin("plugin.jpa") version "1.9.21"
+    kotlin("plugin.allopen") version "1.9.21"
+    kotlin("kapt") version "1.9.21"
 }
 
 group = "com.example"
@@ -15,6 +16,8 @@ version = "0.0.1-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
+
+val queryDslVersion: String by extra
 
 repositories {
     mavenCentral()
@@ -36,6 +39,15 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("com.ninja-squad:springmockk:4.0.0")
+
+    // spring data envers dependency
+    implementation("org.springframework.data:spring-data-envers")
+
+    // Querydsl dependency
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    kapt("jakarta.annotation:jakarta.annotation-api")
+    kapt("jakarta.persistence:jakarta.persistence-api")
 }
 
 tasks.withType<KotlinCompile> {
@@ -53,4 +65,30 @@ allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.Embeddable")
     annotation("jakarta.persistence.MappedSuperclass")
+}
+
+// Querydsl setting
+val generated = file("src/main/generated")
+
+// querydsl QClass 파일 생성 위치를 지정
+tasks.withType<JavaCompile> {
+    options.generatedSourceOutputDirectory.set(generated)
+}
+
+// kotlin source set 에 querydsl QClass 위치 추가
+sourceSets {
+    main {
+        kotlin.srcDirs += generated
+    }
+}
+
+// gradle clean 시에 QClass 디렉토리 삭제
+tasks.named("clean") {
+    doLast {
+        generated.deleteRecursively()
+    }
+}
+
+kapt {
+    generateStubs = true
 }
