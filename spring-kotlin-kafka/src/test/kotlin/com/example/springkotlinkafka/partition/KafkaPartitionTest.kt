3db3c3d05ca3f6d68af312a4f71a66c9352eb159
JJiWoNing <jjiwoning12@gmail.com>
2024-09-02 22:18:27 +0900

    kafka producing key 값 테스트

--- spring-kotlin-kafka/src/test/kotlin/com/example/springkotlinkafka/partition/KafkaPartitionTest.kt
+++ spring-kotlin-kafka/src/test/kotlin/com/example/springkotlinkafka/partition/KafkaPartitionTest.kt
@@ -2,6 +2,7 @@ package com.example.springkotlinkafka.partition
 
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -30,6 +31,25 @@ class KafkaPartitionTest(
         }
     }
 
+    @Test
+    @DisplayName("토픽을 발행할 때 key 값을 맞추면 같은 파티션으로 토픽이 발행된다.")
+    fun test2() {
+        val topic = "test-topic"
+
+        val map = mutableMapOf<Int, Int>()
+
+        repeat(100) {
+            val producerRecord1 = ProducerRecord(topic, getKey(it), "안녕하세요! ${it}")
+            val partition = producer.send(producerRecord1).get().partition()
+            map[partition] = map.getOrDefault(partition, 0) + 1
+        }
+
+        Assertions.assertThat(map).hasSize(2)
+        map.forEach{
+            Assertions.assertThat(it.value).isEqualTo(50)
+        }
+    }
+
     private fun getKafkaProduceProps(): MutableMap<String, Any>? {
         val props = KafkaTestUtils.producerProps(embeddedKafkaBroker)
         props["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
@@ -38,9 +58,9 @@ class KafkaPartitionTest(
 
     private fun getKey(number: Int): String {
         return if (number % 2 == 0) {
-            "key-1"
+            "key-1111"
         } else {
-            "key-2"
+            "key-2222"
         }
     }
 }
